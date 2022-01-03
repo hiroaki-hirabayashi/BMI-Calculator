50fea64cf6ad2d85da6ef8c3ad54b74c83f5bd9d
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2022-01-04 00:43:14 +0900

    リファクタリング

---
+++ BMI-Calculator/ResultViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ResultViewController.swift
+//  BMI-Calculator
+//
+//  Created by Hiroaki-Hirabayashi on 2022/01/04.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    var bmiValue: String?
+    var advice: String?
+    var color: UIColor?
+
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        view.backgroundColor = color
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
+
+
+
